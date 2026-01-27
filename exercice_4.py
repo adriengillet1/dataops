825bcb04fb7090bfc9e8f8f38df7c414533b4b2b
Christophe Blefari <christophe.blefari@gmail.com>
2026-01-27 10:04:56 +0100

    Add all dates from taxi

--- exercice_4.py
+++ exercice_4.py
@@ -4,23 +4,37 @@
 #   "pyarrow",
 #   "google-cloud-storage",
 #   "google-cloud-bigquery",
+#   "requests",
 # ]
 # ///
 
+import datetime
+import requests
 from google.cloud import storage
 from google.cloud import bigquery
 
 client = storage.Client(project="ensai-2026")
 bucket = client.bucket("christophe-2026")
-blob = bucket.blob("yellow_tripdata_2025-01.parquet")
-blob.upload_from_filename("yellow_tripdata_2025-01.parquet")
+
+start = datetime.datetime(2025, 1, 1)
+while start < datetime.datetime(2025, 11, 30):
+    url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{start.strftime('%Y-%m')}.parquet"
+    print("Downloading from: ", url)
+    response = requests.get(url)
+    with open(f"yellow_tripdata_{start.strftime('%Y-%m')}.parquet", "wb") as f:
+        f.write(response.content)
+
+    blob = bucket.blob(f"taxi/month={start.strftime('%Y-%m')}/yellow_tripdata.parquet")
+    blob.upload_from_filename(f"yellow_tripdata_{start.strftime('%Y-%m')}.parquet")
+
+    start += datetime.timedelta(days=31)
 
 client = bigquery.Client(project="ensai-2026")
 sql = """
 CREATE OR REPLACE EXTERNAL TABLE christophe.taxi
 OPTIONS (
-	format = 'PARQUET',
-	uris = ['gs://christophe-2026/yellow_tripdata_2025-01.parquet']
+    format = 'PARQUET',
+    uris = ['gs://christophe-2026/taxi/month=*/yellow_tripdata.parquet']
 );
 """
 
